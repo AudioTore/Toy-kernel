4c4cec062be61c5050e3dab238aff612460d35e2
Thomas <117720256+audiotore482@users.noreply.github.com>
2022-12-01 15:25:51 -0600

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@ export PATH=$PATH:/usr/local/i386elfgcc/bin
 
 nasm "main.asm" -f bin -o "main.bin"
 nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
+nasm "Drivers/Keyboard/IDT2.asm" -f elf -o "IDT2.bin"
 i386-elf-gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "kernel.o"
 nasm "doublebyte.asm" -f bin -o "doublebyte.bin"
 
 i386-elf-ld -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o" --oformat binary
 
-cat "main.bin" "full_kernel.bin" "doublebyte.bin"  > "EchOS.bin"
+cat "main.bin" "full_kernel.bin" "doublebyte.bin" "IDT2.bin" > "EchOS.bin"
 
 echo Building successfull!
 
