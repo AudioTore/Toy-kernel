bc3e38e2dbfddc0f898d0590519eeb45dc4560eb
Thomas <117720256+audiotore482@users.noreply.github.com>
2022-12-01 10:24:21 -0600

    Add files via upload

--- Drivers/Typetext.cpp
+++ Drivers/Typetext.cpp
@@ -1,132 +1,104 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
+#define VGA_CTRL_REGISTER 0x3d4
+#define VGA_DATA_REGISTER 0x3d5
+#define VGA_OFFSET_LOW 0x0f
+#define VGA_OFFSET_HIGH 0x0e
 
-// Color settings.
-enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
-};
-
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
-{
-	return fg | bg << 4;
+unsigned char port_byte_in(unsigned short port) {
+	unsigned char result;
+	__asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+	return result;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
-{
-	return (uint16_t) uc | (uint16_t) color << 8;
+void port_byte_out(unsigned short port, unsigned char data) {
+	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-size_t strlen(const char* str)
-{
-	size_t len = 0;
-	while (str[len])
-		len++;
-	return len;
+
+void set_cursor(int offset) {
+    offset /= 2;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset >> 8));
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset & 0xff));
 }
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+int get_cursor() {
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    int offset = port_byte_in(VGA_DATA_REGISTER) << 8;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    offset += port_byte_in(VGA_DATA_REGISTER);
+    return offset * 2;
+}
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
 
-void terminal_initialize(void)
-{
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
+void set_char_at_video_memory(char character, int offset) {
+    unsigned char *vidmem = (unsigned char *) VIDEO_ADDRESS;
+    vidmem[offset] = character;
+    vidmem[offset + 1] = WHITE_ON_BLACK;
+}
+
+
+int get_row_from_offset(int offset) {
+    return offset / (2 * MAX_COLS);
+}
+
+int get_offset(int col, int row) {
+    return 2 * (row * MAX_COLS + col);
+}
+
+int move_offset_to_new_line(int offset) {
+    return get_offset(0, get_row_from_offset(offset) + 1);
+}
+
+
+// implementing scrolling.
+
+void memory_copy(char *source, char*dest, int nbytes) {
+	int i;
+	for (i = 0; i < nbytes; i++) {
+		*(dest + i) = *(source + i);
 	}
 }
 
-void terminal_setcolor(uint8_t color)
-{
-	terminal_color = color;
+int scroll_ln(int offset) {
+    memory_copy(
+            (char *) (get_offset(0, 1) + VIDEO_ADDRESS),
+            (char *) (get_offset(0, 0) + VIDEO_ADDRESS),
+            MAX_COLS * (MAX_ROWS - 1) * 2
+    );
+
+    for (int col = 0; col < MAX_COLS; col++) {
+        set_char_at_video_memory(' ', get_offset(col, MAX_ROWS - 1));
+    }
+
+    return offset - 2 * MAX_COLS;
 }
 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
-}
-
-void terminal_putchar(char c)
-{
-	/*
-	if (c == '\n') {
-		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-			if (++terminal_column == VGA_WIDTH) {
-			terminal_column = 0;
-			if (++terminal_row == VGA_HEIGHT)
-			{
-				terminal_row = 0;
-			}
+void print_string(char *string) {
+	int offset = get_cursor();
+	int i = 0;
+	while (string[i] != 0) {
+		if (offset >= MAX_ROWS * MAX_COLS * 2) {
+			offset = scroll_ln(offset);
 		}
-	}
-	
-	*/
-	
-	if (c == '\n') {
-		terminal_row++;
-		terminal_column = 0;
-	}else{
-		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-		if (++terminal_column == VGA_WIDTH) {
-			terminal_column = 0;
-			if (++terminal_row == VGA_HEIGHT)
-			{
-				terminal_row = 0;
-			}
-		} 
-	}
-}
-
-
-
-
-
-
-void terminal_write(const char* data, size_t size)
-{
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
-}
-
-void terminal_writestring(const char* data)
-{
-	terminal_write(data, strlen(data));
-}
-
-
-// Code from Richard5656
-void clearscr(){
-		volatile char *video = (volatile char*)0xB8000;
-	        for(int i=0; i<VGA_WIDTH*VGA_HEIGHT;i++){
-			*(terminal_buffer+i) = ' ';
-								
+		if (string[i] == '\n') {
+			offset = move_offset_to_new_line(offset);
+		} else {
+			set_char_at_video_memory(string[i], offset);
+			offset += 2;
 		}
+		i++;
+	}
+	set_cursor(offset);
 }
 
+void clear_screen() {
+	for (int i = 0; i < MAX_COLS * MAX_ROWS; ++i) {
+		set_char_at_video_memory(' ', i * 2);
+	}
+	set_cursor(get_offset(0, 0));
+}
