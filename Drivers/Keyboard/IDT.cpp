0f08978d41bc84a2d56d22dd0cbe7070cf19f966
Thomas <117720256+audiotore482@users.noreply.github.com>
2022-12-02 19:27:25 -0600

    Add files via upload

--- Drivers/Keyboard/IDT.cpp
+++ Drivers/Keyboard/IDT.cpp
@@ -5,6 +5,58 @@
 #define low_16(address) (uint16_t)((address) & 0xFFFF
 #define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
 
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+/* IRQ definitions */
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
+
+
 typedef struct {
 	uint16_t low_offset;
 	uint16_t selector;
@@ -34,7 +86,7 @@ char *exception_messages[] = {
 	
 };
 
-void isr_handler(register *r) {
+void isr_handler(register_t *r) {
 	print_string(exception_messages[r->int_no]);
 	print_nl();
 }
