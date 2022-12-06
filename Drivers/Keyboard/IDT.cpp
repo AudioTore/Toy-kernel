79bd36aaddf35fc445d9fbb42fbec67bfea4de9c
Richard5656 <80961838+Richard5656@users.noreply.github.com>
2022-12-05 17:39:47 -0800

    Add files via upload

--- Drivers/Keyboard/IDT.cpp
+++ Drivers/Keyboard/IDT.cpp
@@ -1,60 +1,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
+#define IDT_ENTRIES 256
 #define low_16(address) (uint16_t)((address) & 0xFFFF
 #define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
-
-extern void isr0();
-extern void isr1();
-extern void isr2();
-extern void isr3();
-extern void isr4();
-extern void isr5();
-extern void isr6();
-extern void isr7();
-extern void isr8();
-extern void isr9();
-extern void isr10();
-extern void isr11();
-extern void isr12();
-extern void isr13();
-extern void isr14();
-extern void isr15();
-extern void isr16();
-extern void isr17();
-extern void isr18();
-extern void isr19();
-extern void isr20();
-extern void isr21();
-extern void isr22();
-extern void isr23();
-extern void isr24();
-extern void isr25();
-extern void isr26();
-extern void isr27();
-extern void isr28();
-extern void isr29();
-extern void isr30();
-extern void isr31();
-/* IRQ definitions */
-extern void irq0();
-extern void irq1();
-extern void irq2();
-extern void irq3();
-extern void irq4();
-extern void irq5();
-extern void irq6();
-extern void irq7();
-extern void irq8();
-extern void irq9();
-extern void irq10();
-extern void irq11();
-extern void irq12();
-extern void irq13();
-extern void irq14();
-extern void irq15();
-
 
 
 typedef struct {
@@ -65,62 +14,126 @@ typedef struct {
 	uint16_t high_offset;
 } __attribute__((packed)) idt_gate_t;
 
+typedef struct {
+	uint32_t ds;
+	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	uint32_t int_no, err_code;
+	uint32_t eip, cs, eflags, useresp, ss;
+} __attribute__((packed)) registers_t;
+
+typedef struct {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed)) idt_registers_t;
+
+extern "C" void isr0();
+extern "C" void isr1();
+extern "C" void isr2();
+extern "C" void isr3();
+extern "C" void isr4();
+extern "C" void isr5();
+extern "C" void isr6();
+extern "C" void isr7();
+extern "C" void isr8();
+extern "C" void isr9();
+extern "C" void isr10();
+extern "C" void isr11();
+extern "C" void isr12();
+extern "C" void isr13();
+extern "C" void isr14();
+extern "C" void isr15();
+extern "C" void isr16();
+extern "C" void isr17();
+extern "C" void isr18();
+extern "C" void isr19();
+extern "C" void isr20();
+extern "C" void isr21();
+extern "C" void isr22();
+extern "C" void isr23();
+extern "C" void isr24();
+extern "C" void isr25();
+extern "C" void isr26();
+extern "C" void isr27();
+extern "C" void isr28();
+extern "C" void isr29();
+extern "C" void isr30();
+extern "C" void isr31();
+/* IRQ definitions */
+extern "C" void irq0();
+extern "C" void irq1();
+extern "C" void irq2();
+extern "C" void irq3();
+extern "C" void irq4();
+extern "C" void irq5();
+extern "C" void irq6();
+extern "C" void irq7();
+extern "C" void irq8();
+extern "C" void irq9();
+extern "C" void irq10();
+extern "C" void irq11();
+extern "C" void irq12();
+extern "C" void irq13();
+extern "C" void irq14();
+extern "C" void irq15();
 
 
+
+idt_registers_t idt_reg;
 idt_gate_t idt[256];
 
-void set_idt_gate(int n, uint32_t handler) {
-	idt[n].low_offset = low_16(handler));
-	idt[n].selector = 0x08;
-	idt[n].always0 = 0;
 
-	idt[n].flags = 0x8E;
-	idt[n].high_offset = high_16(handler);
-}
+
 
 // Error messages.
 char *exception_messages[] = {
 	"Division by zero",
 	"Debug",
 	"Reserved"
-	
 };
 
-void isr_handler(register_t *r) {
-	print_string(exception_messages[r->int_no]);
-	print_nl();
+
+void set_idt_gate(int n, uint32_t handler) {
+	idt[n].low_offset = low_16(handler));
+	idt[n].selector = 0x08;
+	idt[n].always0 = 0;
+	idt[n].flags = 0x8E;
+	idt[n].high_offset = high_16(handler);
 }
 
-typedef struct {
-	uint32_t ds;
-	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-	uint32_t int_no, err_code;
-	uint32_t eip, cs, eflags, useresp, ss;
-} registers_t;
 
-[extern isr_handler]
+
+
+extern "C" void isr_handler(registers_t *r) {
+	print_string(exception_messages[r->int_no]);
+}
+
+
+
+
 
 
 typedef void (*isr_t)(registers_t *);
 
-ist_t interrupt_handlers[256];
+isr_t interrupt_handlers[255];
 
-extern "C" void irq_handler(registers_t *r) {
-	if (interrupt_handlers[r->int_no] != 0) {
-		isr_t handler = interrupt_handlers[r->int_no];
-		handler(r);
-	}
 
-	port_byte_out(0x20, 0x20);
-	if (r->int_no < 40) {
-		port_byte_out(0xA0, 0x20);
-	}
+void  register_interrupt_handler(unsigned char n, isr_t handler) {
+    interrupt_handlers[n] = handler;
 }
 
-typedef struct {
-	uint16_t limit;
-	uint32_t base;
-} __attribute__((packed)) idt_register_t;
+
+extern "C" void irq_handler(registers_t r) {
+
+    if (r.int_no >= 40) port_byte_out(0xA0, 0x20); /* slave */
+    port_byte_out(0x20, 0x20); /* master */
+
+
+    if (interrupt_handlers[r.int_no] != 0) {
+        isr_t handler = interrupt_handlers[r.int_no];
+		handler(&r);
+    }
+}
+
 
 void load_idt() {
 	idt_reg.base = (uint32_t) &idt;
@@ -128,11 +141,41 @@ void load_idt() {
 	asm volatile("lidt (%0)" : : "r"(&idt_reg));
 }
 
-void isr_install() {
-	set_idt_gate(0, (uint32_t) isr0);
-	set_idt_gate(1, (uint32_t) isr1);
+typedef unsigned int u32;
 
-	set_idt_gate(31, (uint32_t) isr31);
+extern "C" void isr_install() {
+    set_idt_gate(0, (u32)isr0);
+    set_idt_gate(1, (u32)isr1);
+    set_idt_gate(2, (u32)isr2);
+    set_idt_gate(3, (u32)isr3);
+    set_idt_gate(4, (u32)isr4);
+    set_idt_gate(5, (u32)isr5);
+    set_idt_gate(6, (u32)isr6);
+    set_idt_gate(7, (u32)isr7);
+    set_idt_gate(8, (u32)isr8);
+    set_idt_gate(9, (u32)isr9);
+    set_idt_gate(10, (u32)isr10);
+    set_idt_gate(11, (u32)isr11);
+    set_idt_gate(12, (u32)isr12);
+    set_idt_gate(13, (u32)isr13);
+    set_idt_gate(14, (u32)isr14);
+    set_idt_gate(15, (u32)isr15);
+    set_idt_gate(16, (u32)isr16);
+    set_idt_gate(17, (u32)isr17);
+    set_idt_gate(18, (u32)isr18);
+    set_idt_gate(19, (u32)isr19);
+    set_idt_gate(20, (u32)isr20);
+    set_idt_gate(21, (u32)isr21);
+    set_idt_gate(22, (u32)isr22);
+    set_idt_gate(23, (u32)isr23);
+    set_idt_gate(24, (u32)isr24);
+    set_idt_gate(25, (u32)isr25);
+    set_idt_gate(26, (u32)isr26);
+    set_idt_gate(27, (u32)isr27);
+    set_idt_gate(28, (u32)isr28);
+    set_idt_gate(29, (u32)isr29);
+    set_idt_gate(30, (u32)isr30);
+    set_idt_gate(31, (u32)isr31);
 
 	port_byte_out(0x20, 0x11);
 	port_byte_out(0xA0, 0x11);
@@ -149,19 +192,23 @@ void isr_install() {
 	port_byte_out(0x21, 0x0);
 	port_byte_out(0xA1, 0x0);
 
-	set_idt_gate(32, (uint32_t)irq0);
+    set_idt_gate(32, (u32)irq0);
+    set_idt_gate(33, (u32)irq1);
+    set_idt_gate(34, (u32)irq2);
+    set_idt_gate(35, (u32)irq3);
+    set_idt_gate(36, (u32)irq4);
+    set_idt_gate(37, (u32)irq5);
+    set_idt_gate(38, (u32)irq6);
+    set_idt_gate(39, (u32)irq7);
+    set_idt_gate(40, (u32)irq8);
+    set_idt_gate(41, (u32)irq9);
+    set_idt_gate(42, (u32)irq10);
+    set_idt_gate(43, (u32)irq11);
+    set_idt_gate(44, (u32)irq12);
+    set_idt_gate(45, (u32)irq13);
+    set_idt_gate(46, (u32)irq14);
+    set_idt_gate(47, (u32)irq15);
 
-	set_idt_gate(39, (uint32_t)irq7);
-
-	set_idt_gate(40, (uint32_t)irq8);
-
-	set_idt_gate(47, (uint32_t)irq15);
 
 	load_idt();
 }
-
-
-
-
-	
-
--- Drivers/Keyboard/Input.cpp
+++ Drivers/Keyboard/Input.cpp
@@ -1,49 +1,102 @@
-#include "IDT.cpp"
+
+#define IRQ1 33
 
 
 
+unsigned char kbdus[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
 
-void print_letter(uint8_t scancode) {
-	switch (scancode) {
-		case 0x0:
-			print_string("ERROR");
-			break;
-		
-		case 0x1:
-			print_string("ESC");
-			break;
+char char_getch;
+int getch_f =0;
 
-		case 0x2:
-			print_string("1");
-			break;
 
-		case 0x3:
-			print_string("2");
-			break;
-
-		case 0x39:
-			print_string("Space");
-			break;
-
-		default:
-			if (scancode <= 0x7f) {
-				print_string("Unknown Key down");
-			} else if (scancode <= 0x39 + 0x80) {
-				print_string("key up");
-				print_letter(scancode - 0x80);
-			} else {
-				print_string("Unknown key up");
-			}
-			break;
+char getch(){
+	getch_f =1;
+	while(getch_f==1){
+	
 	}
+	return char_getch;
 }
+
+
+
+
 
 static void keyboard_callback(registers_t *regs) {
-	uint8_t scancode = port_byte_in(0x60);
-	print_letter(scancode);
-	print_nl();
+	uint8_t scancode = port_byte_in(0x60);				
+				if (scancode <= 56) {
+					getch_f = 0;
+					char_getch = kbdus[scancode];
+				}
 }
 
+
+
+/*
+char buffer[200];
+
+int indexer =0;
+static void keyboard_callback(registers_t *regs) {
+	uint8_t scancode = port_byte_in(0x60);
+	//print_letter(scancode);
+				indexer%=199;
+				
+				if (scancode <= 56 && scancode != 0x1 ) {
+					clear_screen();
+					buffer[indexer++]=kbdus[scancode];
+					print_string(buffer);
+					
+				}else if(scancode == 0x1){
+					
+					indexer =0;
+					for(int i =0; i<199;i++){
+						buffer[i]=' ';
+					}
+					clear_screen();
+				}
+				
+}
+
+*/
 void init_keyboard() {
 	register_interrupt_handler(IRQ1, keyboard_callback);
 }
+
+
