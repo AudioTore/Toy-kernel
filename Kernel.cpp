8ea8fd26008b4241517c82ea19a8c22c497fcafe
Richard5656 <80961838+Richard5656@users.noreply.github.com>
2022-12-05 17:35:16 -0800

    Add files via upload
    New kernel.

--- Kernel.cpp
+++ Kernel.cpp
@@ -1,14 +1,22 @@
 // gcc 7.4.0
 
-#include "Drivers/Typetext.cpp"
-#include "Drivers/Keyboard/IDT.cpp"
+#include "Drivers/Typetext.h"
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+//#include "Drivers/Keyboard/idt.c"
+#include "Drivers/IO.h"
+
+
+
 
 extern "C" void main()
 {
 	
+//while(1){	
+print_string("Hi there bud");
+//}
+//	idt_install();
 }
 
