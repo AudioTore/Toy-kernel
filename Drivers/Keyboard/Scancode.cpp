e969be32277e6ae4f19c3720e5e613ee1261128d
Thomas <117720256+audiotore482@users.noreply.github.com>
2023-02-11 15:37:05 -0600

    Add files via upload

--- Drivers/Keyboard/Scancode.cpp
+++ Drivers/Keyboard/Scancode.cpp
@@ -22,6 +22,46 @@ void append(char s[], char n) {
 }
 
 static char key_buffer[256];
+static char cap_key_buffer[128];
+
+const char scancode_to_shift[] = {
+     0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+  '(', ')', '+', /*'´' */0, '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'Q', 'W', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+ '\'', '<',   0,		/* Left shift */
+ '\\', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+  'M', '<', '>', '?',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,  '<',
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
 
 const char scancode_to_char[] = {
      0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
@@ -62,7 +102,7 @@ const char scancode_to_char[] = {
     0,	/* All other keys are undefined */
 };
 
-
+/*
 void print_letter(uint8_t scancode) {
 	switch (scancode) {
 		case 0x0:
@@ -94,7 +134,7 @@ void print_letter(uint8_t scancode) {
 	}
 }
 
-
+*/
 void print_backspace() {
     int newCursor = get_cursor() - 2;
     set_char_at_video_memory(' ', newCursor);
@@ -133,10 +173,28 @@ void execute_command(char *input) {
 }
 */
 
+bool shift_hold = false;
+
 static void keyboard_callback(registers_t *regs) {
     uint8_t scancode = port_byte_in(0x60);
-    if (scancode > SC_MAX) return;
+    // return scancode_to_shift[scancode];
 
+    // scancode_to_captial[scancode];
+
+    unsigned char c = 0;
+    if (scancode > SC_MAX) return;
+    
+
+    // If shift is pressed.    
+    if (scancode == 0x2A)
+    {
+    	shift_hold = true;
+	print_string("Pressed!");
+    } if (scancode == 0xAA) {
+	shift_hold = false;
+    	print_string("Worked!");
+    }
+    /*
     if (scancode == BACKSPACE) {
         if (backspace(key_buffer) == true) {
             print_backspace();
@@ -146,11 +204,22 @@ static void keyboard_callback(registers_t *regs) {
         execute_command(key_buffer);
         key_buffer[0] = '\0';
     } else {
-        char letter = scancode_to_char[(int) scancode];
-        append(key_buffer, letter);
-        char str[2] = {letter, '\0'};
-        print_string(str);
+	if (shift_hold == true)
+	{
+		char letter = scancode_to_shift[(int) scancode];
+        	append(key_buffer, letter);
+        	char str[2] = {letter, '\0'};
+        	print_string(str);
+	}
+	else if (shift_hold == false)
+	{
+		char letter = scancode_to_char[(int) scancode];
+        	append(key_buffer, letter);
+        	char str[2] = {letter, '\0'};
+        	print_string(str);
+	}
     }
+    */
 }
 
 void init_keyboard() {
