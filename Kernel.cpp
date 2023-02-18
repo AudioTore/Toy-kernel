3ac334fd0e960582a33400d49e6db690124dd13c
Thomas <117720256+audiotore482@users.noreply.github.com>
2023-02-17 20:03:14 -0600

    Add files via upload

--- Kernel.cpp
+++ Kernel.cpp
@@ -1,17 +1,17 @@
 // gcc 7.4.0
 
-// Core part of the OS..
+// Our kernel.
+// Does exactly what a kernel is supposed to do.
+// Setup all our drivers ;)
 
-
-#include "memory.hpp"
 #include "Drivers/Typetext.cpp"
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include "Drivers/Keyboard/IDT.cpp"
 #include "Drivers/Keyboard/Scancode.cpp"
-
+#include "Drivers/ATA/ata.cpp"
+#include "Drivers/fs/readb.cpp"
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -19,21 +19,39 @@
 
 extern "C" void main()
 {
+	 	
+         clear_screen_notext();
+         print_string("Booting EchOS...\n");
+         isr_install();
+
+         asm volatile("sti");
+
+         print_string("Initializing keyboard Driver [PS2] \n");
+         // init_keyboard();
+	 
+	 
+	 uint32_t target;
+	 read_sectors_ATA_PIO(target, 0x0, 1);
+	
+	 int i;
+	 i = 0;
+	 uint32_t bwrite[512];
+	 for (i = 0; i < 512; i++)
+	 {
+	 	bwrite[i] = 0x0;
+	 }
+	 write_sectors_ATA_PIO(0x0, 2, bwrite);
+
+	 print_string("Reading.. \n");
+	 read_sectors_ATA_PIO(target, 0x0, 1);
+	 
+	 clear_screen_notext();
 	 clear_screen();
-    	 print_string("Installing ISRS.. \n");
-   	 isr_install();
+         print_string("EchOS has successfully booted! \n");
+	 print_string("Welcome! \n");
 
-    	 print_string("Enabling ints.. \n");
-    	 asm volatile("sti");
+	 print_string("[C:/]: ");
+         init_keyboard();
 
-   	 print_string("Initializing keyboard (IRQ 1)");
-    	 init_keyboard();
-
-	 clear_screen();
-	 print_string(isolate_args("eeeeeee.s args ();",strlen("eeeeeee.s args ();")));
-	 print_string("\n");
-	 print_string("> ");
-
-    	 
 }
 
--- main.asm
+++ main.asm
@@ -4,6 +4,31 @@ KERNEL_LOCATION equ 0x1000
 [org 0x7c00]
 mov [BootDNum], dl
 
+jmp short end_bpb
+; Jump over data        ;Three bytes off start.
+NOP                   ;If short jump, must have NOP to make 3 bytes
+OEM_ID            db 8 bytes
+BytesPerSector    dw 0x0200        ;512 bytes per sector
+SectorsPerCluster db 0x01          ;1 sector per cluster
+ReservedSectors   dw 0x0001        ;Reserved sectors.. ie boot sector
+TotalFats         db 0x02          ;2 copies of the FAT
+MaxRootEntries    dw 0x0E0         ;Number of entries in the root. 224
+TotalSectors      dw 0x0B40        ;Number of sectors in volume 2880
+MediaDescriptor   db 0xF0          ; 1.44 floppy
+SectorsPerFat     dw 0x0009        ;Number of sectors in a FAT 9
+SectorsPerTrack   dw 0x0012        ;Sectors per Track 18
+NumHeads          dw 0x0002        ;2 heads
+HiddenSectors     dd 0x00000000
+TotalSectorsLarge dd 0x00000000
+DriveNumber       db 0x00
+Flags             db 0x00
+Signature        db 0x29
+VolumeID          dd 0xFFFFFFFF
+VolumeLabel       db "XXXXXXXXXXXX" ;12 bytes 8+"."+3
+SystemID          db "FAT12   "     ;8 bytes
+end_bpb:
+
+
 ; Print "Booting..."
 
 ; B
@@ -56,6 +81,7 @@ mov ah, 0x0e
 mov al, '.'
 int 0x10
 
+
 ; Video memory configuration..
 xor ax, ax
 mov es, ax
@@ -90,6 +116,8 @@ mov ah, 0x0
 mov al, 0x3
 int 0x10
 
+
+
 CODE_SEG equ GDT_code - GDT_start
 DATA_SEG equ GDT_data - GDT_start
 
@@ -101,6 +129,26 @@ mov cr0, eax
 jmp CODE_SEG:start_protected_mode
 
 jmp $
+
+; Video memory configuration..
+xor ax, ax
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, 0x7e00
+
+mov ah, 2
+mov al, 1 ; Sectors we want to read.
+mov ch, 0 ; Cylindar number.
+mov dh, 0 ; Head number.
+mov cl, 2 ; Total amount of sectors.
+mov dl, [BootDNum]
+int 0x13
+
+mov bx, KERNEL_LOCATION
+mov dh, 20
 
 BootDNum: db 0
 
@@ -149,6 +197,9 @@ start_protected_mode:
 		
 	
   jmp KERNEL_LOCATION
+
+
+
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
